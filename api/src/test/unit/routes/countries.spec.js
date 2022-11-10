100016f160f11fb65ae439d4d8094f3b90881f5f
Jau2002 <idamendjaume@gmail.com>
2022-11-10 00:41:30 -0500

    test: get by query

--- api/src/test/unit/routes/countries.spec.js
+++ api/src/test/unit/routes/countries.spec.js
@@ -24,11 +24,49 @@ describe('Country routes', () => {
 	const agent = session(app);
 
 	describe('GET /countries HTTP/1.1', () => {
-		it('should get 200 return as JSON object', async () => {
-			const { status, body, header } = await agent.get('/countries');
-			expect(status).to.equal(200);
+		it('should return as JSON object', async () => {
+			const { body, header } = await agent.get('/countries');
 			expect(header['content-type']).to.match(/application\/json/);
 			expect(body).to.not.be.an('object');
+		});
+
+		it('should when request to send status 200', async () => {
+			const { status } = await agent.get('/countries');
+			expect(status).to.equal(200);
+		});
+
+		it('should return the data when pass a country by query', async () => {
+			const { body } = await agent
+				.get('/countries')
+				.query({ name: 'Colombia' });
+			expect(body).to.deep.equal([
+				{
+					code: 'COL',
+					name: 'Colombia',
+					flag: 'https://flagcdn.com/co.svg',
+					continent: 'South America',
+					capital: 'Bogotá',
+					subregion: 'South America',
+					area: 1141748,
+					population: 50882884,
+				},
+			]);
+		});
+
+		it('should send status 409 the query does not match any country', async () => {
+			const { status } = await agent
+				.get('/countries')
+				.query({ name: 'Polombia' });
+			expect(status).to.equal(409);
+		});
+
+		it('should send error message the query does not match any country', async () => {
+			const { body } = await agent
+				.get('/countries')
+				.query({ name: 'Polombia' });
+			expect(body).to.deep.equal({
+				message: 'the Polombia country does not exist',
+			});
 		});
 	});
 });
