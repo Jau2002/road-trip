c195e6c48f9fd11065ed03f9cbc2a50a356350fb
Jau2002 <idamendjaume@gmail.com>
2022-11-10 00:05:02 -0500

    feat: get all countries

--- api/src/controllers/getByQuery.js
+++ api/src/controllers/getByQuery.js
@@ -1,5 +1,13 @@
+const insertion = require('../utils/insertion');
+const { OK, NOT_FOUND } = require('./protocols');
+
 const getByQuery = async (req, res) => {
-	const { name } = req.query;
+	const allCountries = await insertion();
+	try {
+		return res.status(OK).send(allCountries);
+	} catch (err) {
+		return res.status(NOT_FOUND).send({ message: err.message });
+	}
 };
 
 module.exports = getByQuery;
