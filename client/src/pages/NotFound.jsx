c439e5be0165d28a69a917ca709d42f6724eba13
Jau2002 <idamendjaume@gmail.com>
2022-11-16 14:34:25 -0500

    style: page 404

--- client/src/pages/NotFound.jsx
+++ client/src/pages/NotFound.jsx
@@ -3,14 +3,17 @@ import { Link, useRouteError } from 'react-router-dom';
 function NotFound() {
 	const { status, statusText } = useRouteError();
 	return (
-		<header>
-			<h2>{status}</h2>
-			<p>{statusText}</p>
-			<h3>The page you´re looking for does not exist</h3>
+		<header className='Header-404'>
+			<h2 className='Header-404-h2'>{status}</h2>
+			<p className='Header-404-p'>{statusText}</p>
+			<h3 className='Header-404-h3'>
+				The page you´re looking for does not exist
+			</h3>
 			<Link to='/'>
 				<input
 					type='button'
 					value='Go to Welcome'
+					className='Header-404-input'
 				/>
 			</Link>
 		</header>
